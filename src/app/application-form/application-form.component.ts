accaace2677620d8af3ca0b9dae0b9a0c8fb176d
Shevane Subramaniam <shevsicales@gmail.com>
2024-02-06 15:54:03 -0500

    Application form final changes

--- src/app/application-form/application-form.component.ts
+++ src/app/application-form/application-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { JobService } from '../job.service';
 import { Job } from '../job';
 
@@ -10,7 +11,9 @@ import { Job } from '../job';
 export class ApplicationFormComponent {
 
   // Selected Job Object Instantiation
-  job: Job= new Job();
+  job: Job = new Job();
+
+  @ViewChild('applicationForm', { static: false }) applicationForm!: NgForm; // Add { static: false }
 
   constructor(private jobService: JobService) {}
 
@@ -18,4 +21,36 @@ export class ApplicationFormComponent {
     this.job = this.jobService.getJobObject();
   }
 
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return; // Form will display the error message, no need for an alert
+    }
+
+    // If all fields are filled, you can submit the form
+    const formData = new FormData();
+    formData.append('firstName', form.value.firstName);
+    formData.append('lastName', form.value.lastName);
+    formData.append('email', form.value.email);
+    formData.append('phoneNumber', form.value.phoneNumber);
+    formData.append('address', form.value.address);
+    formData.append('city', form.value.city);
+    formData.append('state', form.value.state);
+    formData.append('zip', form.value.zip);
+    formData.append('resume', form.value.resume);
+    formData.append('linkedin', form.value.linkedin);
+    formData.append('howDidYouHear', form.value.howDidYouHear);
+    formData.append('message', form.value.message);
+
+    // Clear the form
+    this.applicationForm.resetForm();
+
+    setTimeout(() => {
+      this.showSuccessPopup();
+    }, 250);
+  }
+
+  // Method to display success popup
+  showSuccessPopup() {
+    alert('Form submitted successfully!');
+  }
 }
