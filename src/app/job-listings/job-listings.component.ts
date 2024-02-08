05becdcd304b65b32dc480fae02f04d4df183bf6
ShevaneSubramaniam <shevsicales@gmail.com>
2024-02-07 22:28:30 -0500

    update

--- src/app/job-listings/job-listings.component.ts
+++ src/app/job-listings/job-listings.component.ts
@@ -25,34 +25,34 @@ export class JobListingsComponent implements OnInit {
         location: 'New York',
         salary: '$70,000 - $90,000',
         description: 'A hotel manager oversees all operations of a hotel, including hiring and training staff, setting standards for guest services, handling complex tasks and conflicts and establishing and enforcing policies. They also collaborate with other administrators to manage budgets, purchasing, vendor relationships, inventory and sales..',
-        qualifications: 'Bachelor\'s degree in Computer Science, experience with HTML, CSS, and JavaScript.',
+        qualifications: 'Bachelor\'s degree in Hospitality Management or related field preferred.',
         visible: true
       },
       {
         id: 223,
-        title: 'UX Designer',
+        title: 'Assistant hotel manager',
         location: 'San Francisco',
         salary: '$80,000 - $100,000',
-        description: 'Shape the user experience of our products with your creative design skills.',
-        qualifications: 'Bachelor\'s degree in Design, proficiency in design tools like Sketch or Figma.',
+        description: 'An assistant hotel manager supports the hotel manager by completing administrative duties, training new staff, managing correspondence, delegating tasks and supervising staff while the hotel manager is unavailable.',
+        qualifications: 'Bachelor\'s degree in Hospitality Management or related field preferred.',
         visible: true
       },
       {
         id: 455,
-        title: 'Data Analyst',
+        title: 'Director of sales and marketing',
         location: 'Boston',
-        salary: '$60,000 - $80,000',
-        description: 'Turn data into actionable insights and drive business decision-making.',
-        qualifications: 'Bachelor\'s degree in Statistics, experience with data analysis tools.',
+        salary: '$120,000 - $180,000',
+        description: 'A director of sales and marketing is responsible for setting advertising, marketing and sales goals for the hotel. They also oversee marketing teams in creating ads and promotional materials, create and manage a marketing budget and collaborate with the hotel manager to ensure the hotel&#39;s profitability..',
+        qualifications: 'Bachelor\'s degree in Hospitality Management or related field preferred.',
         visible: true
       },
       {
         id: 777,
-        title: 'Data Analyst',
+        title: 'Room attendant',
         location: 'Los Angeles',
         salary: '$60,000 - $80,000',
-        description: 'Turn data into actionable insights and drive business decision-making.',
-        qualifications: 'Bachelor\'s degree in Statistics, experience with data analysis tools.',
+        description: 'A room attendant is responsible for keeping guest rooms clean and stocked with amenities. They may also assist housekeepers in replacing linens and towels and preparing rooms before guests check in..',
+        qualifications: 'High School diploma or GED equivalent.',
         visible: true
       },
       {
@@ -60,8 +60,8 @@ export class JobListingsComponent implements OnInit {
         title: 'Data Analyst',
         location: 'Atlanta',
         salary: '$60,000 - $80,000',
-        description: 'Turn data into actionable insights and drive business decision-making.',
-        qualifications: 'Bachelor\'s degree in Statistics, experience with data analysis tools.',
+        description: 'A parking attendant supervises a hotel parking lot or garage, ensuring security, charging guests for parking and keeping the lot or garage clean.',
+        qualifications: 'High school diploma.',
         visible: true
       },
       {
